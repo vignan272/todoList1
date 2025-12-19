e22895fad722b974f4e38643cc008f12014eff7c
vignan272 <h.vignan100@gmail.com>
2025-12-19 20:04:20 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "react-toastify/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,11 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
